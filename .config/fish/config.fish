559ee13e1468c5094eef5588cd72443b5637aa12
Alejandro Hita <alejandrohitagarrido@gmail.com>
2025-08-08 13:26:14 +0200

    helix alias & no tmux auto start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,16 +5,12 @@ if status is-interactive
     alias l="eza -la"
     alias ll="l -s type"
     alias t="l -T"
+    alias hx="helix"
 
     fish_vi_key_bindings
-
-	if not set -q TMUX
-        tmux new-session
-    end
 end
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 set --export VCPKG_ROOT "$HOME/vcpkg"
-
