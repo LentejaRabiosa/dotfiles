88b25cece912fc047e5d624dbe14d6f36be0ce3d
Alejandro <71976518+LentejaRabiosa@users.noreply.github.com>
2025-09-01 12:23:55 +0200

    oops

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,8 +29,6 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
-	-- { src = "https://github.com/lervag/vimtex" },
-	-- { src = "https://github.com/L3MON4D3/LuaSnip" },
 	{ src = "https://github.com/windwp/nvim-autopairs" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 
@@ -72,22 +70,6 @@ map("n", "-", "<CMD>Oil<CR>")
 
 vim.cmd('filetype plugin indent on')
 vim.g.vimtex_view_method = "zathura"
-
-local ls = require("luasnip")
-ls.config.set_config({
-	enable_autosnippets = true,
-	update_events = "TextChanged,TextChangedI",
-	store_selection_keys = "<C-K>",
-})
-require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
-map({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-map({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
-map({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
-map({ "i", "s" }, "<C-E>", function()
-	if ls.choice_active() then
-		ls.change_choice(1)
-	end
-end, { silent = true })
 
 map("n", "<leader>lf", vim.lsp.buf.format)
 map("n", "<leader>lr", vim.lsp.buf.rename)
