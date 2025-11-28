6bb4d382ac95e4c09e9c1f4f5962e02814347a0e
Alejandro <71976518+LentejaRabiosa@users.noreply.github.com>
2025-11-28 21:23:58 +0100

    Better arch installation script

---
+++ arch-post-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "ARCH POST INSTALL SCRIPT (arch-chroot)"
+
+read -p "Area/Location: " localtime
+ln -sf /usr/share/zoneinfo/$localtime /etc/localtime
+hwclock --systohc
+
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=es" > /etc/vconsole.conf
+
+read -p "hostname: " hostname
+sed -i "/127.0.1.1/d" /etc/hosts
+echo "127.0.1.1        $hostname" >> /etc/hosts
+
+passwd
+read -p "user: " user
+useradd -mG wheel -s /bin/bash $user
+passwd $user
+
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+pacman -Sy --noconfirm
+pacman -S --noconfirm neovim git sway foot firefox eza fd fzf ripgrep networkmanager brightnessctl grub os-prober efibootmgr
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
+os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
+echo "POST INSTALL COMPLETE"
