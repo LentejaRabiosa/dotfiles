45ac77ac87c7bd02ad023a23afca0e39b39d9fd5
Alejandro <71976518+LentejaRabiosa@users.noreply.github.com>
2025-12-01 23:33:11 +0100

    JetBrains Mono Nerd Font

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -24,7 +24,7 @@ passwd $user
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 pacman -Sy --noconfirm
-pacman -S --noconfirm neovim git sway foot firefox eza fd less fzf ripgrep networkmanager openssh brightnessctl grub os-prober efibootmgr
+pacman -S --noconfirm neovim git sway foot firefox eza fd less fzf ripgrep networkmanager openssh brightnessctl grub os-prober efibootmgr ttf-jetbrains-mono-nerd
 
 ssh-keygen -t ed25519 -C $hostname
 eval "$(ssh-agent -s)"
