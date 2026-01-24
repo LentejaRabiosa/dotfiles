2b1de329610971b3e98eb0338609ee3d42540e8d
Alejandro <71976518+LentejaRabiosa@users.noreply.github.com>
2026-01-25 00:55:37 +0100

    kanso theme and block cursor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,11 @@ if status is-interactive
     alias hx="helix"
 
     fish_vi_key_bindings
+
+    set fish_cursor_default     block      blink
+    set fish_cursor_insert      block      blink
+    set fish_cursor_replace_one underscore blink
+    set fish_cursor_visual      block
 end
 
 # bun
@@ -15,37 +20,3 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 set --export VCPKG_ROOT "$HOME/vcpkg"
 set --export EDITOR nvim
-
-# Kanagawa Fish shell theme
-set -l foreground DCD7BA normal
-set -l selection 2D4F67 brcyan
-set -l comment 727169 brblack
-set -l red C34043 red
-set -l orange FF9E64 brred
-set -l yellow C0A36E yellow
-set -l green 76946A green
-set -l purple 957FB8 magenta
-set -l cyan 7AA89F cyan
-set -l pink D27E99 brmagenta
-
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
